f60cd5ef7de62b4f5aa0d8259c3c5a2a3cfa5e5c
Jeff Swartz <jeff.swartz@vonage.com>
2024-09-13 11:12:43 -0700

    pass activity into OTCamera2VideoCapturer()

--- android/src/main/java/com/opentokreactnative/OTSessionManager.java
+++ android/src/main/java/com/opentokreactnative/OTSessionManager.java
@@ -183,9 +183,9 @@ public class OTSessionManager extends ReactContextBaseJavaModule
                     .build();
             mPublisher.setPublisherVideoType(PublisherKit.PublisherKitVideoType.PublisherKitVideoTypeScreen);
         } else {
-            View view = getCurrentActivity().getWindow().getDecorView().getRootView();
-            Context displayContext = this.getReactApplicationContext().getDisplayContext(view.getDisplay());
-            OTCamera2VideoCapturer capturer = new OTCamera2VideoCapturer(displayContext, Publisher.CameraCaptureResolution.valueOf(resolution), Publisher.CameraCaptureFrameRate.valueOf(frameRate));
+            // View view = getCurrentActivity().getWindow().getDecorView().getRootView();
+            // Context displayContext = this.getReactApplicationContext().getDisplayContext(view.getDisplay());
+            OTCamera2VideoCapturer capturer = new OTCamera2VideoCapturer(getCurrentActivity(), Publisher.CameraCaptureResolution.valueOf(resolution), Publisher.CameraCaptureFrameRate.valueOf(frameRate));
             mPublisher = new Publisher.Builder(this.getReactApplicationContext())
                     .audioTrack(audioTrack)
                     .videoTrack(videoTrack)
