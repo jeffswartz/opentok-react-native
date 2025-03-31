d635a3a1d09c4cf01a807ea06857491f9100fff5
Jeff Swartz <jeff.swartz@vonage.com>
2025-03-31 15:05:21 -0700

    lint fixes

--- src/helpers/OTPublisherHelper.js
+++ src/helpers/OTPublisherHelper.js
@@ -1,4 +1,4 @@
-import { sanitizeBooleanProperty, reassignEvents } from './OTHelper';
+import { sanitizeBooleanProperty } from './OTHelper';
 
 const sanitizeResolution = (resolution) => {
   switch (resolution) {
